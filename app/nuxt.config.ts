cf857e880b4e409d3b10bde335af7620e73f2d92
Aabill <domingoaaronbill@gmail.com>
2023-10-13 15:53:58 +0800

    remove unused dependencies and update nuxt config to use env for baseUrl

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -11,15 +11,8 @@ export default defineNuxtConfig({
 	},
 	modules: ['@hebilicious/authjs-nuxt'],    
 	runtimeConfig: {
-		authJs: {
-			verifyClientOnEveryRequest: true,
-			guestRedirectTo: "/login", // where to redirect if the user is not authenticated
-			authenticatedRedirectTo: "/", // where to redirect if the user is authenticated
-			baseUrl: "https://aabill-nuxt.netlify.app" // should be something like https://www.my-app.com
-		},
     authJs: {
       secret: process.env.NUXT_NEXTAUTH_SECRET,
-			baseUrl: 'https://aabill-nuxt.netlify.app/' // You can generate one with `openssl rand -base64 32`
     },
     github: {
       clientId: process.env.NUXT_GITHUB_CLIENT_ID,
@@ -27,7 +20,7 @@ export default defineNuxtConfig({
     },
     public: {
       authJs: {
-        baseUrl: 'https://aabill-nuxt.netlify.app/', // The URL of your deployed app (used for origin Check in production)
+        baseUrl: process.env.NUXT_NEXTAUTH_URL, // The URL of your deployed app (used for origin Check in production)
         verifyClientOnEveryRequest: true, // whether to hit the /auth/session endpoint on every client request
 				guestRedirectTo: '/login'
       }
@@ -36,20 +29,4 @@ export default defineNuxtConfig({
 	alias: {
     cookie: resolve(__dirname, "node_modules/cookie")
   }
-	// modules: ["@hebilicious/authjs-nuxt"],
-	// runtimeConfig: {
-	// 	authJs: {
-	// 		secret: 'testing123'
-	// 	},
-	// 	github: {
-	// 		clientId: 'e22dbcf3cf1010832860',
-	// 		clientSecret: '8666cda04af34dc4ab4bec03afb70f198221411b'
-	// 	},
-	// 	public: {
-	// 		authJs: {
-	// 			baseUrl: 'http://localhost:3001',
-	// 			verifyClientOnEveryRequest: true
-	// 		}
-	// 	}
-	// }
 })
